c03e37d9329557cde944827331e793bbce959389
akhilnv3535 <akhilnv35@gmail.com>
2025-04-13 15:05:11 +0530

    Remove hero model and api's

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from starlette.middleware.cors import CORSMiddleware
 
 from core.db import engine
 from route.category_route import cat_route
-from route.hero_route import route as hero_route
+# from route.hero_route import route as hero_route
 from route.models import SQLModel
 
 SQLModel.metadata.create_all(engine)
@@ -18,5 +18,5 @@ app.add_middleware(
     allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
 )
 
-app.include_router(hero_route)
+# app.include_router(hero_route)
 app.include_router(cat_route)
--- route/models.py
+++ route/models.py
@@ -8,17 +8,17 @@ def now_utc():
     return datetime.now(timezone.utc)
 
 
-class Hero(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
-    age: int | None = Field(default=None, index=True)
-    secret_name: Optional[str] = None
-    created: Optional[datetime] = Field(
-        default_factory=lambda: datetime.now(timezone.utc)
-    )
-    modified: Optional[datetime] = Field(
-        default_factory=lambda: datetime.now(timezone.utc)
-    )
+# class Hero(SQLModel, table=True):
+#     id: Optional[int] = Field(default=None, primary_key=True)
+#     name: str = Field(index=True)
+#     age: int | None = Field(default=None, index=True)
+#     secret_name: Optional[str] = None
+#     created: Optional[datetime] = Field(
+#         default_factory=lambda: datetime.now(timezone.utc)
+#     )
+#     modified: Optional[datetime] = Field(
+#         default_factory=lambda: datetime.now(timezone.utc)
+#     )
 
 
 class SubCategory(SQLModel, table=True):
