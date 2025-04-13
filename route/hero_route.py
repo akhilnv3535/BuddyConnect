105e012cb98550437c586e87b4584e7c7785d3ac
akhilnv3535 <akhilnv35@gmail.com>
2025-04-13 15:05:11 +0530

    Change datetime function to utils file

--- route/hero_route.py
+++ route/hero_route.py
@@ -1,48 +1,48 @@
-from typing import Annotated
-
-from fastapi import APIRouter
-from fastapi import HTTPException, Query
-
-from core.db import SessionDep
-from route.models import Hero
-from route.schemas import HeroSchema
-
-route = APIRouter(prefix="/hero", tags=['Hero'])
-
-
-@route.post("/heroes/")
-def create_hero(hero: HeroSchema, session: SessionDep) -> Hero:
-    hero = Hero(**hero.model_dump())
-    session.add(hero)
-    session.commit()
-    session.refresh(hero)
-    return hero
-
-
-@route.get("/heroes/")
-def read_heroes(
-        session: SessionDep,
-        offset: int = 0,
-        limit: Annotated[int, Query(le=100)] = 100,
-) -> list[Hero]:
-    heroes = session.query(Hero).offset(offset).limit(limit).all()
-    # heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
-    return heroes
-
-
-@route.get("/heroes/{hero_id}")
-def read_hero(hero_id: int, session: SessionDep) -> Hero:
-    hero = session.get(Hero, hero_id)
-    if not hero:
-        raise HTTPException(status_code=404, detail="Hero not found")
-    return hero
-
-
-@route.delete("/heroes/{hero_id}")
-def delete_hero(hero_id: int, session: SessionDep):
-    hero = session.get(Hero, hero_id)
-    if not hero:
-        raise HTTPException(status_code=404, detail="Hero not found")
-    session.delete(hero)
-    session.commit()
-    return {"ok": True}
+# from typing import Annotated
+#
+# from fastapi import APIRouter
+# from fastapi import HTTPException, Query
+#
+# from core.db import SessionDep
+# from route.models import Hero
+# from route.schemas import HeroSchema
+#
+# route = APIRouter(prefix="/hero", tags=['Hero'])
+#
+#
+# @route.post("/heroes/")
+# def create_hero(hero: HeroSchema, session: SessionDep) -> Hero:
+#     hero = Hero(**hero.model_dump())
+#     session.add(hero)
+#     session.commit()
+#     session.refresh(hero)
+#     return hero
+#
+#
+# @route.get("/heroes/")
+# def read_heroes(
+#         session: SessionDep,
+#         offset: int = 0,
+#         limit: Annotated[int, Query(le=100)] = 100,
+# ) -> list[Hero]:
+#     heroes = session.query(Hero).offset(offset).limit(limit).all()
+#     # heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+#     return heroes
+#
+#
+# @route.get("/heroes/{hero_id}")
+# def read_hero(hero_id: int, session: SessionDep) -> Hero:
+#     hero = session.get(Hero, hero_id)
+#     if not hero:
+#         raise HTTPException(status_code=404, detail="Hero not found")
+#     return hero
+#
+#
+# @route.delete("/heroes/{hero_id}")
+# def delete_hero(hero_id: int, session: SessionDep):
+#     hero = session.get(Hero, hero_id)
+#     if not hero:
+#         raise HTTPException(status_code=404, detail="Hero not found")
+#     session.delete(hero)
+#     session.commit()
+#     return {"ok": True}
--- route/models.py
+++ route/models.py
@@ -1,24 +1,22 @@
-from datetime import timezone, datetime
+from datetime import datetime
 from typing import Optional, List
 
 from sqlmodel import SQLModel, Field, Relationship
 
-
-def now_utc():
-    return datetime.now(timezone.utc)
+from utils import now_utc
 
 
-class Hero(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
-    age: int | None = Field(default=None, index=True)
-    secret_name: Optional[str] = None
-    created: Optional[datetime] = Field(
-        default_factory=lambda: datetime.now(timezone.utc)
-    )
-    modified: Optional[datetime] = Field(
-        default_factory=lambda: datetime.now(timezone.utc)
-    )
+# class Hero(SQLModel, table=True):
+#     id: Optional[int] = Field(default=None, primary_key=True)
+#     name: str = Field(index=True)
+#     age: int | None = Field(default=None, index=True)
+#     secret_name: Optional[str] = None
+#     created: Optional[datetime] = Field(
+#         default_factory=lambda: datetime.now(timezone.utc)
+#     )
+#     modified: Optional[datetime] = Field(
+#         default_factory=lambda: datetime.now(timezone.utc)
+#     )
 
 
 class SubCategory(SQLModel, table=True):
@@ -27,10 +25,10 @@ class SubCategory(SQLModel, table=True):
     category_id: Optional[int] = Field(default=None, foreign_key="category.id")
     category: Optional["Category"] = Relationship(back_populates="subcategories")
     created: Optional[datetime] = Field(
-        default_factory=lambda: datetime.now(timezone.utc)
+        default_factory=lambda: now_utc()
     )
     modified: Optional[datetime] = Field(
-        default_factory=lambda: datetime.now(timezone.utc)
+        default_factory=lambda: now_utc()
     )
 
 
@@ -39,10 +37,10 @@ class Category(SQLModel, table=True):
     name: str
     subcategories: List[SubCategory] = Relationship(back_populates="category")
     created: Optional[datetime] = Field(
-        default_factory=lambda: datetime.now(timezone.utc)
+        default_factory=lambda: now_utc()
     )
     modified: Optional[datetime] = Field(
-        default_factory=lambda: datetime.now(timezone.utc)
+        default_factory=lambda: now_utc()
     )
 
 
